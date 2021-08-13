09f78fbb78fedae9a3d8536107c63d4323f1b4c5
Hernandez Puente <violeta.hp@outlook.com>
2021-08-12 23:24:26 -0500

    SQL exercises updated.

--- SQL_EXERCISES_2ND_SET_10-08-2021.sql
+++ SQL_EXERCISES_2ND_SET_10-08-2021.sql
@@ -15,35 +15,37 @@ ORDER BY DEPARTMENT_ID ASC;
 #not in the range $10,000 through $15,000 and are in department 30 or 100.
 SELECT FIRST_NAME, LAST_NAME, SALARY, DEPARTMENT_ID
 FROM labhr.employees
-WHERE SALARY NOT BETWEEN 10000 AND 15000 AND DEPARTMENT_ID IN (30, 100)
+WHERE SALARY NOT BETWEEN 10000 AND 15000 
+AND DEPARTMENT_ID IN (30, 100);
 
 #4.Write a query to display the name (first_name, last_name) and hire date for all employees who were
 # hired in 1987.
+#Hint: Google the usage of the SQL function YEAR().
+SELECT FIRST_NAME, LAST_NAME, HIRE_DATE
+FROM labhr.employees
+WHERE YEAR(HIRE_DATE)=1987;
+
 SELECT FIRST_NAME, LAST_NAME, HIRE_DATE
 FROM labhr.employees
 WHERE HIRE_DATE LIKE '%1987%'; #Have 1987 in any position 
 #LIKE seeks for a specified pattern in a column.
 
-SELECT FIRST_NAME, LAST_NAME, HIRE_DATE
-FROM labhr.employees
-WHERE YEAR(HIRE_DATE)=1987;
-
-#Hint: Google the usage of the SQL function YEAR().
 
 #5.Write a query to display the first_name of all employees who have both "b" and "c" in their first name.
 SELECT FIRST_NAME
 FROM labhr.employees
-WHERE FIRST_NAME LIKE 'b%' 
-AND FIRST_NAME LIKE 'c%';
+WHERE FIRST_NAME LIKE '%b%' 
+AND FIRST_NAME LIKE '%c%';
 
 #6.Write a query to display the last name, job, and salary for all employees whose job is that of a
 # Programmer or a Shipping Clerk, and whose salary is not equal to $4,500, $10,000, or $15,000.
 SELECT E.LAST_NAME,J.JOB_TITLE, E.SALARY
 FROM labhr.employees E 
-JOIN labhr.jobs J ON E.JOB_ID= J.JOB_ID
-WHERE JOB_TITLE='Programmer' OR JOB_TITLE='Shipping Clerk' 
-AND SALARY NOT IN ('4500', '10000' OR '5000')
+INNER JOIN labhr.jobs J ON E.JOB_ID= J.JOB_ID
+WHERE (JOB_TITLE='Programmer' OR JOB_TITLE='Shipping Clerk') 
+AND SALARY NOT IN (4500, 10000, 5000);
 
+DESCRIBE labhr.employees;
 
 #7.	Write a query to display the last name of employees whose names have exactly 6 characters.
 #Hint: Google the usage of the SQL function CHARACTER_LENGTH().
@@ -61,9 +63,17 @@ SELECT DISTINCT E.JOB_ID, J.JOB_TITLE
 FROM labhr.employees E 
 JOIN labhr.jobs J ON E.JOB_ID= J.JOB_ID;
 
+SELECT E.JOB_ID, J.JOB_TITLE
+FROM labhr.employees E 
+JOIN labhr.jobs J ON E.JOB_ID= J.JOB_ID
+GROUP BY JOB_TITLE;
+
 #10.	Write a query to display the name (first_name, last_name), salary and PF (calculate as 15% of 
 #salary) of all employees.
 SELECT FIRST_NAME, LAST_NAME, SALARY, SALARY*0.15 AS PF 
+FROM labhr.employees;
+
+SELECT CONCAT(FIRST_NAME, " ", LAST_NAME) AS "Name", SALARY, SALARY*0.15 AS PF 
 FROM labhr.employees;
 
 #11.	Write a query to select all record from employees where last name in 'BLAKE', 'SCOTT', 'KING' and
@@ -71,3 +81,10 @@ FROM labhr.employees;
 SELECT * 
 FROM labhr.employees
 WHERE LAST_NAME IN ('BLAKE', 'SCOTT', 'KING', 'FORD');
+
+SELECT * 
+FROM labhr.employees
+WHERE LAST_NAME ='BLAKE' 
+OR LAST_NAME ='SCOTT'
+OR LAST_NAME ='KING' 
+OR LAST_NAME ='FORD';
---
+++ SQL_EXERCISES_4TH_SET_12-08-2021.sql
@@ -0,0 +1,115 @@
+SET sql_mode = 'ONLY_FULL_GROUP_BY';
+
+#1.Write a query to find the addresses (location_id, street_address, city, state_province, country_name) 
+#of all the departments.
+SELECT LOCATION_ID, STREET_ADDRESS, CITY, STATE_PROVINCE, C.COUNTRY_NAME
+FROM locations L
+JOIN countries C ON L.COUNTRY_ID= C.COUNTRY_ID;
+
+#2.Write a query to get the total salaries payable to employees.
+SELECT SUM(SALARY)
+FROM employees;
+
+#3. Write a query to find the name (first_name, last name), department ID and name of all the employees.
+SELECT E.FIRST_NAME, E.LAST_NAME, E.DEPARTMENT_ID, D.DEPARTMENT_NAME
+FROM employees E
+JOIN departments D ON E.DEPARTMENT_ID= D.DEPARTMENT_ID;
+
+#3.Write a query to find the name (first_name, last_name), job, department ID and name of the
+#employees who works in London.
+SELECT E.FIRST_NAME, E.LAST_NAME, J.JOB_ID, D.DEPARTMENT_NAME, L.CITY
+FROM employees E
+JOIN departments D ON E.DEPARTMENT_ID= D.DEPARTMENT_ID
+JOIN jobs J ON E.JOB_ID=J.JOB_ID 
+JOIN locations L ON L.LOCATION_ID = D.LOCATION_ID
+WHERE CITY="LONDON";
+
+#4.Write a query to find the employee id, name (last_name) along with their manager_id and NAME 
+#(last_name).
+SELECT E.EMPLOYEE_ID, E.LAST_NAME, E.MANAGER_ID, D.DEPARTMENT_NAME
+FROM employees E
+JOIN departments D ON E.DEPARTMENT_ID= D.DEPARTMENT_ID;
+
+#5.Write a query to find the name (first_name, last_name) and hire date of the employees who was 
+#hired after 'Jones'.
+SELECT LAST_NAME, LAST_NAME, HIRE_DATE
+FROM employees 
+WHERE HIRE_DATE> (SELECT HIRE_DATE FROM employees WHERE LAST_NAME='Jones')
+ORDER BY HIRE_DATE
+
+#6.Write a query to get the department name and number of employees in the department.
+SELECT D.DEPARTMENT_NAME, COUNT(E.EMPLOYEE_ID) AS "NUMBER OF EMPLOYEES"
+FROM employees E
+JOIN departments D ON E.DEPARTMENT_ID= D.DEPARTMENT_ID
+GROUP BY DEPARTMENT_NAME
+ORDER BY DEPARTMENT_NAME;
+
+
+#7.Write a query to find the employee ID, job title, number of days between ending date and starting 
+#date for all jobs in department 90.
+
+SELECT E.EMPLOYEE_ID, J.JOB_TITLE, END_DATE - START_DATE AS DAYS, E.DEPARTMENT_ID 
+FROM employees E
+JOIN jobs J ON E.JOB_ID= J.JOB_ID
+JOIN job_history JH ON E.EMPLOYEE_ID= JH.DEPARTMENT_ID
+WHERE E.DEPARTMENT_ID=90;
+
+
+#8.Write a query to display the department ID and name and first name of manager.
+SELECT E.DEPARTMENT_ID, D.DEPARTMENT_NAME, E.FIRST_NAME, J.JOB_TITLE 
+FROM departments D
+JOIN employees E ON E.MANAGER_ID=E.MANAGER_ID
+JOIN jobs J ON E.JOB_ID= J.JOB_ID
+WHERE J.JOB_TITLE LIKE '%MANAGER%';
+
+#9.Write a query to display the department name, manager name, and city.
+SELECT D.DEPARTMENT_NAME, CONCAT(E.FIRST_NAME," ",E.LAST_NAME) AS 'NAME', J.JOB_TITLE, L.CITY 
+FROM departments D
+JOIN employees E ON E.MANAGER_ID=E.MANAGER_ID
+JOIN jobs J ON E.JOB_ID= J.JOB_ID
+JOIN locations L ON D.LOCATION_ID= L.LOCATION_ID
+WHERE J.JOB_TITLE LIKE '%MANAGER%';
+
+#10.Write a query to display the job title and average salary of employees.
+SELECT J.JOB_TITLE, AVG(SALARY) AS SALARY
+FROM employees E
+JOIN jobs J ON E.JOB_ID= J.JOB_ID
+GROUP BY J.JOB_TITLE;
+
+
+#11.Write a query to display job title, employee name, and the difference between salary of the 
+#employee and minimum salary for the job.
+SELECT J.JOB_TITLE, CONCAT(E.FIRST_NAME," ",E.LAST_NAME) AS 'NAME', SALARY, MIN(SALARY) AS MIN_SALARY,
+(SALARY-MIN(SALARY)) AS SALARY_DIFF
+FROM employees E
+JOIN jobs J ON E.JOB_ID= J.JOB_ID
+GROUP BY J.JOB_ID;
+
+#12.Write a query to display the job history that were done by any employee who is currently drawing 
+#more than 10000 of salary.
+SELECT JH.JOB_ID, JH.START_DATE, JH.END_DATE, JH.DEPARTMENT_ID, 
+CONCAT(E.FIRST_NAME," ",E.LAST_NAME) AS 'NAME', SALARY
+FROM employees E
+JOIN job_history JH ON E.EMPLOYEE_ID= JH.EMPLOYEE_ID
+WHERE SALARY>10000;
+
+SELECT JH.* 
+FROM job_history JH
+JOIN employees E
+ON (JH.EMPLOYEE_ID = E.EMPLOYEE_ID) 
+WHERE SALARY > 10000;
+
+#13.Write a query to display department name, name (first_name, last_name), hire date, salary of 
+#the manager for all managers whose experience is more than 15 years.
+SELECT D.DEPARTMENT_NAME, CONCAT(E.FIRST_NAME," ",E.LAST_NAME) AS 'NAME', E.HIRE_DATE, J.JOB_TITLE,
+SALARY, (DATEDIFF(NOW(),HIRE_DATE))/365>15 AS 'Exp of more than 15 y/o'
+FROM departments D
+JOIN employees E ON E.MANAGER_ID=E.MANAGER_ID
+JOIN jobs J ON E.JOB_ID= J.JOB_ID #to see the manager title
+WHERE J.JOB_TITLE LIKE '%MANAGER%' 
+
+SELECT DEPARTMENT_NAME, FIRST_NAME, SALARY  
+FROM departments D 
+JOIN employees E ON D.MANAGER_ID=E.MANAGER_ID
+WHERE (SYSDATE()-HIRE_DATE) / 365 > 15;
+
